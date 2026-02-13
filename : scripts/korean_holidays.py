f2365d81ed46270443677ecad5a0a6e33be03ffa
ignoramus <yujy118@gmail.com>
2026-02-13 14:31:24 +0900

    Update korean_holidays.py

--- : scripts/korean_holidays.py
+++ : scripts/korean_holidays.py
@@ -1,1 +1,23 @@
+import holidays
+from datetime import date
 
+
+def get_kr_holidays(year=None):
+    if year is None:
+        year = date.today().year
+    return holidays.KR(years=year)
+
+
+def is_korean_holiday(d):
+    kr = get_kr_holidays(d.year)
+    if d in kr:
+        return True, kr[d]
+    return False, ""
+
+
+def is_business_day(d):
+    if d.weekday() >= 5:
+        return False
+    if is_korean_holiday(d)[0]:
+        return False
+    return True
